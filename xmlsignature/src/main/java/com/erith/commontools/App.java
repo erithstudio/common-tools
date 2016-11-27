783dcbae0c141589f3d5a556f2c906deac87e04c
erithstudio <erith.studio@gmail.com>
2016-11-27 14:42:09 +0700

    Init project
    Init commontools project and add security module

---
+++ xmlsignature/src/main/java/com/erith/commontools/App.java
@@ -0,0 +1,13 @@
+package com.erith.commontools;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
